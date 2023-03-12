ee181ea5c7cf20dcddcd7d481fd0cd4a2a5046c9
tr1ten <shubhi.blog@gmail.com>
2023-03-12 22:02:16 +0530

    Added nested routes navigators

---
+++ app/(tabs)/mycustomers.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MyCustomer = () => {
+  return (
+    <View>
+      <Text>MyCustomer</Text>
+    </View>
+  )
+}
+
+export default MyCustomer
