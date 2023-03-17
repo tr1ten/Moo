38daf1dc669011c63ede79b3e4f63563f8345c6b
tr1ten <shubhi.blog@gmail.com>
2023-03-17 23:03:27 +0530

    revert: native wind 2

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo","nativewind/babel"],
+    presets: ["babel-preset-expo"],
     plugins: [
       require.resolve("expo-router/babel"),
       'react-native-reanimated/plugin',
