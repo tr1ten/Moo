18f5b0bad2b8cd3d21b1fac8377a5a47d4d14a78
JaiSehgal007 <jaisehgal511@gmail.com>
2023-04-11 14:25:30 +0530

    ModifiedLogin

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,14 +1,14 @@
-import { ImageBackground, TextInput, View } from "react-native";
+import { ImageBackground, SafeAreaView, ScrollView, TextInput, View } from "react-native";
 import { Input, Text } from "@rneui/themed";
 import React, { useEffect } from "react";
 import { auth } from "../../firebase/firebaseConfig";
 import { StyleSheet } from "react-native";
-import { Button, Card } from "@rneui/base";
+import { Button, Card} from "@rneui/base";
+import { CheckBox } from "react-native-elements";
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { color } from "react-native-reanimated";
 import { registerUser } from "../../services/user";
-import Background from "./Background";
 
 export default function Signin() {
   const [isRegiser, setIsRegister] = React.useState(true);
@@ -22,7 +22,7 @@ export default function Signin() {
     useSignInWithEmailAndPassword(auth);
   const [createUserWithEmailAndPassword, user, loading, userError] =
     useCreateUserWithEmailAndPassword(auth);
-
+  const [isSeller, setIsSeller] = React.useState(0);
   const onRegister = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     if (error) setError("");
@@ -57,7 +57,30 @@ export default function Signin() {
   },[loading,SignLoading]);
 
   return (
-    <Background>
+    <ScrollView
+
+        showsVerticalScrollIndicator={false}
+        style={{
+            flex: 1,
+            backgroundColor:'#607A00',
+        }}
+        contentContainerStyle={{
+            flexGrow: 1,
+        }} 
+        automaticallyAdjustKeyboardInsets={true}
+      >
+      <ImageBackground 
+                source={require("../../assets/images/cow.jpg")} 
+                style={{
+                    width:"100%",
+                    height:"50%",
+                    position:'absolute',
+                    top:0
+                }}
+                imageStyle={{
+                    resizeMode: "cover",
+                }}
+      />
       <Text style={styles.welcomeText}>Welcome To</Text>
       <Text style={styles.Moo}>Moo!</Text>
       <View style={styles.InBack}>
@@ -65,8 +88,28 @@ export default function Signin() {
         {isRegiser ? <Text style={styles.TopText}>Create a{"\n"}new account</Text> : <Text style={styles.TopText}>Login To Your{"\n"}Account</Text>}  
 
         <TextInput style={styles.FieldStyle} editable={!isLoading} selectTextOnFocus={!isLoading} placeholder="Email" value={mail} onChangeText={setMail} placeholderTextColor='#101626' />
-        <TextInput style={styles.FieldStyle} editable={!isLoading} selectTextOnFocus={!isLoading} placeholder="Password" value={password} onChangeText={setPassword} placeholderTextColor='#101626' />
+        <TextInput secureTextEntry={true} style={styles.FieldStyle} editable={!isLoading} selectTextOnFocus={!isLoading} placeholder="Password" value={password} onChangeText={setPassword} placeholderTextColor='#101626' />
         {isRegiser && (<TextInput style={styles.FieldStyle} editable={!isLoading} selectTextOnFocus={!isLoading} placeholder="Confirm Password" value={confirm} onChangeText={setConfirm} placeholderTextColor='#101626' />)}
+        <View style={styles.utype} >
+          <Text 
+            style={styles.utext}
+          >You are</Text>
+          <CheckBox
+            checked={isSeller === 0}
+            
+            onPress={() => setIsSeller(0)}
+            checkedIcon="dot-circle-o"
+            uncheckedIcon="circle-o"
+            title={"Seller"}
+          />
+          <CheckBox
+            checked={isSeller === 1}
+            onPress={() => setIsSeller(1)}
+            checkedIcon="dot-circle-o"
+            uncheckedIcon="circle-o"
+            title={"Buyer"}
+          />
+        </View>
         <Text style={styles.error}>{error}</Text>
             <Button
               loading={isLoading}
@@ -80,7 +123,7 @@ export default function Signin() {
             </Text>
         </Text>
       </View>
-    </Background>
+    </ScrollView>
   );
 }
 const styles = StyleSheet.create({
@@ -137,5 +180,19 @@ const styles = StyleSheet.create({
     backgroundColor:'rgb(220,220,220)',
     height:40,
     marginVertical:20,
-  }
+  },
+  utype: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    backgroundColor:"#DCFFFF",
+  },
+  utext:{
+    fontSize: 17,
+    fontWeight: "bold",
+    backgroundColor:"#DCFFFF",
+  },
 });
+
+
+
