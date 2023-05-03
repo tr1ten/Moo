c07fa0890082b77f5027612dedb01e0b2a41795a
tr1ten <shubhi.blog@gmail.com>
2023-05-03 12:01:06 +0530

    feat: remove expo constants

--- constants/common.ts
+++ constants/common.ts
@@ -1,6 +1,6 @@
-import Constants from "expo-constants";
+// import Constants from "expo-constants";
 
-export const API_URL = Constants?.manifest?.extra?.apiKey;
+export const API_URL = "http://172.16.166.243:3000";
 export const SELLER = 1;
 
 export const BUYER = 2;
--- services/common.test.ts
+++ services/common.test.ts
@@ -6,6 +6,7 @@ import { getItemTypes } from "./common";
 describe('test services/common', function() {
     it('test getItemTypes', async function() {
         const res = await getItemTypes();
+        console.log("recieved item types ",res);
         expect(res).not.null;
     });
 });
